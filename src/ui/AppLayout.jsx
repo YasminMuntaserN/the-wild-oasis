851a7b4e3fa124d946fd9522e00aa87e6c559f5c
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-07 13:31:52 +0300

    feat: add CreateCabinForm component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,29 +1,39 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const StyledAppLayout=styled.div`
-//   display: grid;
-//   grid-template-columns:26rem 1fr;
-//   grid-template-rows:auto 1fr;
-//   height :100vh;
-// `;
+const StyledAppLayout=styled.div`
+  display: grid;
+  grid-template-columns:26rem 1fr;
+  grid-template-rows:auto 1fr;
+  height :100vh;
+`;
 
-// const Main =styled.main`
-//   background-color: green;
-//   pad
-// `;
+const Main =styled.main`
+  background-color: var(--color-grey-50);
+  padding:4rem;
+`;
+
+const Container=styled.div`
+  max-width: 120rem;
+  margin:0 auto;
+  display:flex;
+  flex-direction: column;
+  gap:2.3rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header/>
       <Sidebar/>
-      <main>
+      <Main>
+        <Container>
           <Outlet/>
-      </main>
-    </div>
+        </Container>
+      </Main>
+    </StyledAppLayout>
   )
 }
 
