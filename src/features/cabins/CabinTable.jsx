cee95fd2e62b287dec3bf1f5e5f4c23f88187fec
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-07 09:26:15 +0300

    feat: add Spinner component

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -34,7 +34,7 @@ function CabinTable() {
     data: cabins,
     error,
   } = useQuery({
-    queryKey: ["cabins"],
+    queryKey: ['cabin'],
     queryFn: getCabins,
   });
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function(){
-    getCabins().then((data)=>console.log(data));
-  },[])
   return (
     <>
+    <Row>
         <Heading as="h1">All cabins</Heading>
         <p>Test</p>
 
+      </Row>
+      <Row>
+        <CabinTable/>
       </Row>
     </>
   );
