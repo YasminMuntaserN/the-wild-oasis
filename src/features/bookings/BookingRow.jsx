2245be33012f313d25b6c01888cfab4dc10f424c
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-13 09:15:59 +0300

    refactor: edit the booking row

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 import { format, isToday } from "date-fns";
-// import {
-//   HiArrowDownOnSquare,
-//   HiArrowUpOnSquare,
-//   HiEye,
-//   HiTrash,
-// } from "react-icons/hi2";
-// import { useNavigate } from "react-router-dom";
+import {
+  // HiArrowDownOnSquare,
+  // HiArrowUpOnSquare,
+  HiEye,
+  // HiTrash,
+} from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 import Tag from "../../ui/Tag";
 import Table from "../../ui/Table";
 import Modal from "../../ui/Modal";
-// import Menus from "../../ui/Menus";
+import Menus from "../../ui/Menus";
 // import ConfirmDelete from "../../ui/ConfirmDelete";
 
 import { formatCurrency } from "../../utils/helpers";
@@ -60,7 +60,7 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   // const { checkout, isCheckingOut } = useCheckout();
   // const { deleteBooking, isDeleting } = useDeleteBooking();
 
@@ -97,7 +97,7 @@ function BookingRow({
       <Amount>{formatCurrency(totalPrice)}</Amount>
 
         <Modal>
-       {/* <Menus.Menu>
+       <Menus.Menu>
           <Menus.Toggle id={bookingId} />
           <Menus.List id={bookingId}>
             <Menus.Button
@@ -107,7 +107,7 @@ function BookingRow({
               See details
             </Menus.Button>
 
-            {status === "unconfirmed" && (
+            {/* {status === "unconfirmed" && (
               <Menus.Button
                 icon={<HiArrowDownOnSquare />}
                 onClick={() => navigate(`/checkin/${bookingId}`)}
@@ -130,15 +130,17 @@ function BookingRow({
               <Menus.Button icon={<HiTrash />}>Delete booking</Menus.Button>
             </Modal.Open>
           </Menus.List>
-        </Menus.Menu> */}
+        </Menus.Menu> 
 
-        {/* <Modal.Window name="delete">
+        <Modal.Window name="delete">
           <ConfirmDelete
             resourceName="booking"
             disabled={isDeleting}
             onConfirm={() => deleteBooking(bookingId)}
           />
         </Modal.Window> */}
+                  </Menus.List>
+                  </Menus.Menu> 
       </Modal>
     </Table.Row>
   );
